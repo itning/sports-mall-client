fac2758f5d0870c8a568eb60169c353d0eb671e6
itning <itning@itning.top>
2020-02-12 13:05:19 +0800

    add sider

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,11 @@ export const SERVER_HOST = "http://localhost:8888";
 export const API = {
   user: {
     login: `${SERVER_HOST}/login`
+  },
+  commodityType: {
+    all: `${SERVER_HOST}/commodity_types`
+  },
+  commodity: {
+    type: `${SERVER_HOST}/commodity/type`
   }
 };
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <a-layout class="box-shadow" style="padding: 24px 0; background: #fff">
-      <a-layout-sider collapsedWidth="0" style="background: #fff" v-model="collapsed">
-        <LayoutSider/>
+      <a-layout-sider collapsedWidth="0" style="background: #fff" v-if="showSider">
+        <LayoutSider :data="commodityTypeData"/>
       </a-layout-sider>
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
         <CarouselTwo/>
@@ -16,11 +16,34 @@
   import CarouselTwo from "../components/CarouselTwo";
   import LayoutSider from "../components/LayoutSider";
   import RecommendedProducts from "../components/RecommendedProducts";
+  import {Get} from "../http";
+  import {API} from "../api";
 
   export default {
     name: "MainView",
     components: {RecommendedProducts, CarouselTwo, LayoutSider},
-    data: () => ({collapsed: false})
+    data: () => ({
+      commodityTypeData: [],
+      showSider: true
+    }),
+    methods: {
+      initAllCommodityType() {
+        Get(API.commodityType.all)
+          .withSuccessCode(200)
+          .withErrorStartMsg("加载分类失败：")
+          .do(response => {
+            if (response.data.data.length === 0) {
+              this.showSider = false;
+            } else {
+              this.showSider = true;
+              this.commodityTypeData = response.data.data;
+            }
+          })
+      }
+    },
+    created() {
+      this.initAllCommodityType();
+    }
   }
 </script>
 
