bf5340740690ea6a4ced1ab995cdb9ed30acd703
itning <itning@itning.top>
2020-02-29 11:28:11 +0800

    change to class

--- src/http/index.js
+++ src/http/index.js
@@ -120,148 +120,152 @@ export function Download(url, fileName) {
   });
 }
 
-function _request(url, method) {
-  this.method = method;
-  this.url = url;
-  this.code = 200;
-  this.startMsg = '错误：';
+class _request {
+  constructor(url, method) {
+    this.method = method;
+    this.url = url;
+    this.code = 200;
+    this.startMsg = '错误：';
+  }
+
+  withErrorStartMsg(msg) {
+    this.startMsg = msg;
+    return this;
+  };
+
+  withSuccessCode(code) {
+    this.code = code;
+    return this;
+  };
+
+  withJSONData(params) {
+    this.jsonData = params;
+    return this;
+  };
+
+  withURLSearchParams(params) {
+    let urlSearchParams = new URLSearchParams();
+    for (let p in params) {
+      if (params.hasOwnProperty(p)) {
+        let v = params[p];
+        urlSearchParams.append(p, v);
+      }
+    }
+    this.urlSearchParams = urlSearchParams;
+    return this;
+  };
+
+
+  withFormData(params, enableUploadProgress = true, uploadProgress) {
+    let formData = new FormData();
+    for (let p in params) {
+      if (params.hasOwnProperty(p)) {
+        let v = params[p];
+        formData.append(p, v);
+      }
+    }
+    this.formData = formData;
+    this.enableUploadProgress = enableUploadProgress;
+    this.uploadProgress = uploadProgress;
+    return this;
+  };
+
+  doAfter(fn) {
+    this.doAfterFun = fn;
+    return this;
+  };
+
+  withOnceErrorToast(isOnce = false) {
+    this.isOnce = isOnce;
+    return this;
+  };
+
+  watchError(errorWatchFunc) {
+    this.errorWatchFunc = errorWatchFunc;
+    return this;
+  };
+
+  do(fn) {
+    let that = this;
+    let promise = null;
+    switch (this.method) {
+      case Method.GET:
+        promise = instance.get(this.url);
+        break;
+      case Method.DELETE:
+        if (this.urlSearchParams !== undefined) {
+          promise = instance.delete(this.url, {data: this.urlSearchParams});
+        } else if (this.formData !== undefined) {
+          promise = instance.delete(this.url, {
+            data: this.formData,
+            headers: {'content-type': 'multipart/form-data'}
+          });
+        } else {
+          promise = instance.delete(this.url);
+        }
+        break;
+      case Method.POST:
+        if (this.urlSearchParams !== undefined) {
+          promise = instance.post(this.url, this.urlSearchParams);
+        } else if (this.formData !== undefined) {
+          if (this.enableUploadProgress === false) {
+            promise = instance.post(this.url, this.formData, {headers: {'content-type': 'multipart/form-data'}});
+          } else {
+            const uploadProgress = this.uploadProgress;
+            let config = {
+              headers: {'content-type': 'multipart/form-data'},
+              onUploadProgress: progressEvent => {
+                let progress = progressEvent.loaded / progressEvent.total * 100 | 0;
+                uploadProgress(progress);
+              }
+            };
+            promise = instance.post(this.url, this.formData, config);
+          }
+        } else {
+          promise = instance.post(this.url);
+        }
+        break;
+      case Method.PATCH:
+        if (this.urlSearchParams !== undefined) {
+          promise = instance.patch(this.url, this.urlSearchParams);
+        } else if (this.formData !== undefined) {
+          promise = instance.patch(this.url, this.formData, {headers: {'content-type': 'multipart/form-data'}});
+        } else if (this.jsonData !== undefined) {
+          promise = instance.patch(this.url, this.jsonData);
+        } else {
+          promise = instance.patch(this.url);
+        }
+        break;
+      default:
+        return Promise.reject('none switch found of ' + this.method);
+    }
+    promise
+      .then(response => {
+        if (response.status === that.code) {
+          fn(response)
+        } else {
+          showErrorToast(that.startMsg, response.data.msg);
+        }
+      })
+      .catch(error => {
+        if (error.response !== undefined) {
+          if (that.errorWatchFunc !== undefined) {
+            that.errorWatchFunc(error.response)
+          }
+          if (that.isOnce !== undefined && that.isOnce) {
+            onceToast(error.response.data.msg, that.startMsg);
+          } else {
+            showErrorToast(that.startMsg, error.response.data.msg);
+          }
+        }
+      })
+      .then(() => {
+        if (that.doAfterFun !== undefined) {
+          that.doAfterFun();
+        }
+      });
+    return this;
+  };
 }
-
-_request.prototype.withErrorStartMsg = function (msg) {
-  this.startMsg = msg;
-  return this;
-};
-
-_request.prototype.withSuccessCode = function (code) {
-  this.code = code;
-  return this;
-};
-_request.prototype.withJSONData = function (params) {
-  this.jsonData = params;
-  return this;
-};
-
-_request.prototype.withURLSearchParams = function (params) {
-  let urlSearchParams = new URLSearchParams();
-  for (let p in params) {
-    if (params.hasOwnProperty(p)) {
-      let v = params[p];
-      urlSearchParams.append(p, v);
-    }
-  }
-  this.urlSearchParams = urlSearchParams;
-  return this;
-};
-
-_request.prototype.withFormData = function (params, enableUploadProgress = true, uploadProgress) {
-  let formData = new FormData();
-  for (let p in params) {
-    if (params.hasOwnProperty(p)) {
-      let v = params[p];
-      formData.append(p, v);
-    }
-  }
-  this.formData = formData;
-  this.enableUploadProgress = enableUploadProgress;
-  this.uploadProgress = uploadProgress;
-  return this;
-};
-
-_request.prototype.doAfter = function (fn) {
-  this.doAfterFun = fn;
-  return this;
-};
-
-_request.prototype.withOnceErrorToast = function (isOnce = false) {
-  this.isOnce = isOnce;
-  return this;
-};
-
-_request.prototype.watchError = function (errorWatchFunc) {
-  this.errorWatchFunc = errorWatchFunc;
-  return this;
-};
-
-_request.prototype.do = function (fn) {
-  let that = this;
-  let promise = null;
-  switch (this.method) {
-    case Method.GET:
-      promise = instance.get(this.url);
-      break;
-    case Method.DELETE:
-      if (this.urlSearchParams !== undefined) {
-        promise = instance.delete(this.url, {data: this.urlSearchParams});
-      } else if (this.formData !== undefined) {
-        promise = instance.delete(this.url, {
-          data: this.formData,
-          headers: {'content-type': 'multipart/form-data'}
-        });
-      } else {
-        promise = instance.delete(this.url);
-      }
-      break;
-    case Method.POST:
-      if (this.urlSearchParams !== undefined) {
-        promise = instance.post(this.url, this.urlSearchParams);
-      } else if (this.formData !== undefined) {
-        if (this.enableUploadProgress === false) {
-          promise = instance.post(this.url, this.formData, {headers: {'content-type': 'multipart/form-data'}});
-        } else {
-          const uploadProgress = this.uploadProgress;
-          let config = {
-            headers: {'content-type': 'multipart/form-data'},
-            onUploadProgress: progressEvent => {
-              let progress = progressEvent.loaded / progressEvent.total * 100 | 0;
-              uploadProgress(progress);
-            }
-          };
-          promise = instance.post(this.url, this.formData, config);
-        }
-      } else {
-        promise = instance.post(this.url);
-      }
-      break;
-    case Method.PATCH:
-      if (this.urlSearchParams !== undefined) {
-        promise = instance.patch(this.url, this.urlSearchParams);
-      } else if (this.formData !== undefined) {
-        promise = instance.patch(this.url, this.formData, {headers: {'content-type': 'multipart/form-data'}});
-      } else if (this.jsonData !== undefined) {
-        promise = instance.patch(this.url, this.jsonData);
-      } else {
-        promise = instance.patch(this.url);
-      }
-      break;
-    default:
-      return Promise.reject('none switch found of ' + this.method);
-  }
-  promise
-    .then(response => {
-      if (response.status === that.code) {
-        fn(response)
-      } else {
-        showErrorToast(that.startMsg, response.data.msg);
-      }
-    })
-    .catch(error => {
-      if (error.response !== undefined) {
-        if (that.errorWatchFunc !== undefined) {
-          that.errorWatchFunc(error.response)
-        }
-        if (that.isOnce !== undefined && that.isOnce) {
-          onceToast(error.response.data.msg, that.startMsg);
-        } else {
-          showErrorToast(that.startMsg, error.response.data.msg);
-        }
-      }
-    })
-    .then(() => {
-      if (that.doAfterFun !== undefined) {
-        that.doAfterFun();
-      }
-    });
-  return this;
-};
 
 export default instance;
